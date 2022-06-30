84e31dbd9f25cfd8e0aa93129761e8f31b93a8b1
BurakAskan <gusaskbu@student.gu.se>
2022-06-30 19:08:30 +0200

    Working with ButtonObjects instead of copy-pasted pushButtonObjects.

--- OpenApplications/ButtonObjects.py
+++ OpenApplications/ButtonObjects.py
@@ -1,18 +1,20 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import subprocess
 
+from PyQt5.QtWidgets import *
 
-class ButtonObject(object):
-    def __init__(self, appName, appNumber, executeLink):
+
+class ButtonObject(QPushButton):
+    def __init__(self, appName, appNumber, executeLink, ui):
+        super().__init__(ui)
+
         self.appName = appName
         self.appNumber = appNumber
         self.executeLink = executeLink
 
-        self = QtWidgets.QPushButton(self.centralwidget)
-        self.setGeometry(QtCore.QRect(appNumber*100 + 50 , 100, 100, 100))
+        self.setGeometry(QtCore.QRect(appNumber * 100 + 50, 100, 100, 100))
         self.setAutoFillBackground(False)
-        self.setStyleSheet("border-image : url(logoImages/"+appName+".png);")
-        self.clicked.connect(self.execute)
+        self.setStyleSheet("border-image : url(logoImages/" + appName + ".png);")
 
     def execute(self):
         subprocess.Popen(self.executeLink)
--- OpenApplications/main.py
+++ OpenApplications/main.py
@@ -1,47 +1,32 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-import subprocess
+from OpenApplications import ButtonObjects as BO
 
 
 class Ui_MainWindow(object):
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("Application Opener")
         MainWindow.resize(800, 300)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         MainWindow.setWindowIcon(QtGui.QIcon('logoImages/programming.png'))
 
-        self.steamButton = QtWidgets.QPushButton(self.centralwidget)
-        self.steamButton.setGeometry(QtCore.QRect(50, 100, 100, 100))
-        self.steamButton.setAutoFillBackground(False)
-        self.steamButton.setStyleSheet("border-image : url(logoImages/steam.png);")
-        self.steamButton.clicked.connect(self.steam)
+        self.leagueButton = BO.ButtonObject("league", 4.5, "L://Steam//Riot Games//Riot Client//RiotClientServices.exe", self.centralwidget)
+        self.leagueButton.clicked.connect(lambda : self.leagueButton.execute())
 
-        self.discordButton = QtWidgets.QPushButton(self.centralwidget)
-        self.discordButton.setGeometry(QtCore.QRect(200, 100, 100, 100))
-        self.discordButton.setAutoFillBackground(False)
-        self.discordButton.setStyleSheet("border-image : url(logoImages/discord.png);")
-        self.discordButton.clicked.connect(self.discord)
+        self.discordButton = BO.ButtonObject("discord", 1.5, "C://Users//burak//AppData//Local//Discord//app-1.0.9005//Discord.exe", self.centralwidget)
+        self.discordButton.clicked.connect(lambda : self.discordButton.execute())
 
-        self.chromeButton = QtWidgets.QPushButton(self.centralwidget)
-        self.chromeButton.setGeometry(QtCore.QRect(350, 100, 100, 100))
-        self.chromeButton.setAutoFillBackground(False)
-        self.chromeButton.setStyleSheet("border-image : url(logoImages/chrome.png);")
-        self.chromeButton.clicked.connect(self.chrome)
+        self.steamButton = BO.ButtonObject("steam", 0, "L://Steam//steam.exe", self.centralwidget)
+        self.steamButton.clicked.connect(lambda : self.steamButton.execute())
 
-        self.leagueButton = QtWidgets.QPushButton(self.centralwidget)
-        self.leagueButton.setGeometry(QtCore.QRect(500, 100, 100, 100))
-        self.leagueButton.setAutoFillBackground(False)
-        self.leagueButton.setStyleSheet("border-image : url(logoImages/league.png);")
-        self.leagueButton.clicked.connect(self.league)
+        self.chromeButton = BO.ButtonObject("chrome", 3, "C://Program Files (x86)//Google//Chrome//Application//chrome.exe", self.centralwidget)
+        self.chromeButton.clicked.connect(lambda : self.chromeButton.execute())
 
-        self.pycharmButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pycharmButton.setGeometry(QtCore.QRect(650, 100, 100, 100))
-        self.pycharmButton.setAutoFillBackground(False)
-        self.pycharmButton.setStyleSheet("border-image : url(logoImages/pycharm.png);")
-        self.pycharmButton.clicked.connect(self.pycharm)
-
-
+        self.pycharmButton = BO.ButtonObject("pycharm", 6, "L://Intellij//PyCharm 2022.1.2//bin//pycharm64.exe", self.centralwidget)
+        self.leagueButton.clicked.connect(lambda: self.pycharmButton.execute())
 
         self.eventLabel = QtWidgets.QLabel(self.centralwidget)
         self.eventLabel.setGeometry(QtCore.QRect(340, 20, 91, 41))
@@ -58,30 +43,6 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def discord(self):
-        subprocess.Popen("C://Users//burak//AppData//Local//Discord//app-1.0.9005//Discord.exe")
-        self.eventLabel.setText("You've Changed!")
-
-    def steam(self):
-        subprocess.Popen("L://Steam//steam.exe")
-        self.eventLabel.setText("You should not do this!")
-        self.eventLabel.adjustSize()
-
-    def chrome(self):
-        subprocess.Popen("C://Program Files (x86)//Google//Chrome//Application//chrome.exe")
-        self.eventLabel.setText("Chrome Opened!")
-        self.eventLabel.adjustSize()
-
-    def league(self):
-        subprocess.Popen("L://Steam//Riot Games//Riot Client//RiotClientServices.exe")
-        self.eventLabel.setText("League Opened!")
-        self.eventLabel.adjustSize()
-
-    def pycharm(self):
-        subprocess.Popen("L://Intellij//PyCharm 2022.1.2//bin//pycharm64.exe")
-        self.eventLabel.setText("Pycharm Opened!")
-        self.eventLabel.adjustSize()
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
